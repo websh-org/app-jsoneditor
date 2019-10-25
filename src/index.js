f6ae53b930152e6215ab24fe0cd9b82538254035
Zoran Obradović <zockyo@gmail.com>
2019-10-25 03:39:47 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import { WebShellApp } from "@websh/web-shell-app";
+import JSONEditor from "jsoneditor";
+import "jsoneditor/dist/jsoneditor.min.css";
+import manifest from "./manifest.json";
+
+const container = document.getElementById("editor")
+const options = {}
+const editor = new JSONEditor(container, options)
+
+WebShellApp.manifest(manifest)
+  .command('file-open', ({ format, content, type, extension }) => {
+    editor.set(JSON.parse(content));
+    return "OK";
+  })
+  .command('file-save', ({ format, extension }) => {
+    return { content: editor.get(), type:"application/json" }
+  })
+  .command('file-new', function ({ format }) {
+    editor.set({});
+    return "OK";
+  })
